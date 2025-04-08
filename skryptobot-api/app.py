4e55a4750ee215e70a2ac9d3a3b4937a1e773567
ViitoJooj <joaovitor819oqures@gmail.com>
2025-04-08 19:43:45 -0300

    m

--- skryptobot-api/app.py
+++ skryptobot-api/app.py
@@ -1,10 +1,30 @@
-import uvicorn
+import asyncio
 from insert_data import insert_data
 from predict_data import fetch_and_store_data
-from run_api import app
+
+async def run_insert_data():
+    print("Running insert_data()...")
+    loop = asyncio.get_event_loop()
+    await loop.run_in_executor(None, insert_data)
+    print("Finished insert_data()")
+
+async def run_fetch_and_store_data():
+    print("Running fetch_and_store_data()...")
+    loop = asyncio.get_event_loop()
+    await loop.run_in_executor(None, fetch_and_store_data)
+    print("Finished fetch_and_store_data()")
+
+async def main_1():
+    print("Starting the application...")
+    await run_insert_data()
+
+async def main_2():
+    print("Inserting data...")
+    await run_fetch_and_store_data()
+    print("Fetching and storing data...")
+
+async def main():
+    await asyncio.gather(main_1(), main_2())
 
 if __name__ == "__main__":
-    insert_data()
-    fetch_and_store_data()
-
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    asyncio.run(main())
