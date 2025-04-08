9221c93f3177abedde6bc91aeb380d650373cdfb
ViitoJooj <joaovitor819oqures@gmail.com>
2025-04-08 19:49:53 -0300

    fix api and app

--- skryptobot-api/app.py
+++ skryptobot-api/app.py
@@ -1,30 +1,35 @@
 import asyncio
 from insert_data import insert_data
 from predict_data import fetch_and_store_data
+from fastapi import FastAPI
+from supabase import create_client, Client
+from dotenv import load_dotenv
+import os
 
+app = FastAPI()
+
+# Função para inserir dados
 async def run_insert_data():
     print("Running insert_data()...")
     loop = asyncio.get_event_loop()
     await loop.run_in_executor(None, insert_data)
     print("Finished insert_data()")
 
+# Função para buscar e armazenar dados
 async def run_fetch_and_store_data():
     print("Running fetch_and_store_data()...")
     loop = asyncio.get_event_loop()
     await loop.run_in_executor(None, fetch_and_store_data)
     print("Finished fetch_and_store_data()")
 
-async def main_1():
+# Função de inicialização
+@app.on_event("startup")
+async def startup():
     print("Starting the application...")
-    await run_insert_data()
+    # Executar ambas as funções assíncronas
+    await asyncio.gather(run_insert_data(), run_fetch_and_store_data())
 
-async def main_2():
-    print("Inserting data...")
-    await run_fetch_and_store_data()
-    print("Fetching and storing data...")
-
-async def main():
-    await asyncio.gather(main_1(), main_2())
-
-if __name__ == "__main__":
-    asyncio.run(main())
+# Rota para testar
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
